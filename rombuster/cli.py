d029dfad804cafc1bc79af8917bf01eea202ad30
Ivan Nikolsky <enty8080@gmail.com>
2021-05-20 07:35:28 +0000

    Update and rename core/cli.py to rombuster/cli.py

--- rombuster/cli.py
+++ rombuster/cli.py
@@ -26,10 +26,10 @@
 
 import argparse
 
-from core.exploit import Exploit
+from .exploit import Exploit
 
 
-class CLI(Exploit):
+class RomBusterCLI(Exploit):
     description = "RomBuster is a RomPager exploitation tool that allows to disclosure network router admin password."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--ip-list', dest='list', help='IP addresses list.')
