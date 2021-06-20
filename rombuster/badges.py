847f9765bad14081b503082d862152783055a884
Ivan Nikolsky <enty8080@gmail.com>
2021-06-20 20:28:30 +0200

    Update badges.py

--- rombuster/badges.py
+++ rombuster/badges.py
@@ -24,23 +24,28 @@
 # SOFTWARE.
 #
 
+
 class Badges:
     @staticmethod
-    def print_process(message):
-        print(f"\033[1;34m[*]\033[0m {message}")
+    def print_empty(message="", end='\n'):
+        print(f"\033[1K\r{message}", end=end)
 
     @staticmethod
-    def print_success(message):
-        print(f"\033[1;32m[+]\033[0m {message}")
+    def print_process(message, end='\n'):
+        print(f"\033[1K\r\033[1;34m[*]\033[0m {message}", end=end)
 
     @staticmethod
-    def print_error(message):
-        print(f"\033[1;31m[-]\033[0m {message}")
+    def print_success(message, end='\n'):
+        print(f"\033[1K\r\033[1;32m[+]\033[0m {message}", end=end)
 
     @staticmethod
-    def print_warning(message):
-        print(f"\033[1;33m[!]\033[0m {message}")
+    def print_error(message, end='\n'):
+        print(f"\033[1K\r\033[1;31m[-]\033[0m {message}", end=end)
 
     @staticmethod
-    def print_information(message):
-        print(f"\033[1;77m[i]\033[0m {message}")
+    def print_warning(message, end='\n'):
+        print(f"\033[1K\r\033[1;33m[!]\033[0m {message}", end=end)
+
+    @staticmethod
+    def print_information(message, end='\n'):
+        print(f"\033[1K\r\033[1;77m[i]\033[0m {message}", end=end)
