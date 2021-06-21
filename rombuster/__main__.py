2980025bce370b67146e768fc51ebe2db16212c3
Ivan Nikolsky <enty8080@gmail.com>
2021-06-21 15:09:37 +0200

    Update __main__.py

--- rombuster/__main__.py
+++ rombuster/__main__.py
@@ -44,8 +44,11 @@ class RomBuster:
 
         username = 'admin'
         if response.status_code == 200:
-            data = response.content[8568:]
-            result, window = LZSDecompress(data, RingList(2048))
+            try:
+                data = response.content[8568:]
+                result, window = LZSDecompress(data, RingList(2048))
+            except Exception:
+                return None
 
             password = re.findall("([\040-\176]{5,})", result)
             if len(password):
